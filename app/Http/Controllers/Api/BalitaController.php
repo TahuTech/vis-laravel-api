99d3c444e4c706a543658b166c69c7e90b77f578
TahuTech <18053036@student.uwp.ac.id>
2022-06-27 09:52:51 +0700

    balita api

---
+++ app/Http/Controllers/Api/BalitaController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\BalitaResource;
+use App\Models\balita;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class BalitaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new BalitaResource(balita::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $result = [];
+        $data = ($request->json("data"));
+        //dd($data[0]["provinsi"]);
+        //set validation
+        if (count($data) == 0) {
+            $validator = Validator::make($request->all(), [
+                'provinsi'   => 'required'
+            ]);
+
+
+            //response error validation
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
+        } else {
+            for ($d = 0; $d < count($data); $d++) {
+                //save to database
+                $balita = balita::create([
+                    'balita'     => $data[$d]["balita"]
+                ]);
+                $p = [
+                    'name' => 'Insert balita',
+                    'balita' => $data[$d]["balita"],
+                    'status' => 'success', 'code' => 200
+                ];
+                array_push($result, $p);
+            }
+        }
+
+
+        return new BalitaResource($result);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($balita)
+    {
+        return new BalitaResource($balita);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BalitaResource $balita)
+    {
+        $result = [];
+        $data = ($request->json("data"));
+
+        if (count($data) == 0) {
+            //set validation
+            $validator = Validator::make($request->all(), [
+                'balita'   => 'required'
+            ]);
+
+            //response error validation
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
+        }
+
+        //update to database
+        $balita->update([
+            'balita'     => $data[0]["balita"]
+        ]);
+
+        $p = [
+            'name' => 'Update balita',
+            'balita' => $data[0]["balita"],
+            'status' => 'success', 'code' => 200
+        ];
+        array_push($result, $p);
+
+        return new BalitaResource($result);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BalitaResource $balita)
+    {
+        $balita->delete();
+
+        return new BalitaResource($balita);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -25,27 +25,32 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 /**
  * route resource desa
-*/
+ */
 Route::apiResource('/user', App\Http\Controllers\Api\UserController::class);
 Route::get('/userlogin', 'App\Http\Controllers\Api\UserController@getuserlogin');
 Route::put('/user', 'App\Http\Controllers\Api\UserController@update');
 /**
  * route resource provinsi
-*/
+ */
 Route::apiResource('/provinsi', App\Http\Controllers\Api\ProvinsiController::class);
 /**
  * route resource kabupaten
-*/
+ */
 Route::apiResource('/kabupaten', App\Http\Controllers\Api\KabupatenController::class);
 /**
  * route resource kota
-*/
+ */
 Route::apiResource('/kota', App\Http\Controllers\Api\KotaController::class);
 /**
  * route resource kecamatan
-*/
+ */
 Route::apiResource('/kecamatan', App\Http\Controllers\Api\KecamatanController::class);
 /**
  * route resource desa
-*/
+ */
 Route::apiResource('/desa', App\Http\Controllers\Api\DesaController::class);
+
+/**
+ * route resource balita
+ */
+Route::apiResource('/balita', App\Http\Controllers\Api\BalitaController::class);
